aba8c0ba229652bbe487b2376542c0e214dd91d0
123 <themakariku@gmail.com>
2025-12-08 20:40:40 +0300

    Added snake to the are

--- src/conf.lua
+++ src/conf.lua
@@ -5,15 +5,32 @@
 
 Conf = {};
 
-Conf.width = 70;
-Conf.height= 30;
+--Snake game's area width
+Conf.width = 64;
 
+--Snake game's area height
+Conf.height= 32;
+
+---Snake game's space char
 Conf.space = "@";
+
+---Snake game's space color
 Conf.space_color = Colors.white;
 
+---Snake game's snake's head char
 Conf.snake_head = "*";
-Conf.snake_body = "%";
+
+---Snake game's snake's body
+Conf.snake_body = "0";
+
+---Snake game's snake's color
 Conf.snake_color = Colors.green;
 
+---Snake game's apple char
 Conf.apple = "^";
+
+---Snake game's apple's color
 Conf.apple_color = Colors.red;
+
+---Snake game's snake start length
+Conf.snake_length = 3;
--- src/main.lua
+++ src/main.lua
@@ -3,12 +3,15 @@
     Under MIT
 ]]
 
-package.path = package.path .. ";map/?.lua;" .. "map/init.lua";
+package.path = package.path .. ";map/?.lua;" .. "map/init.lua;";
 
 require "colors";
 require "conf";
 require "cords";
 require "map";
+require "elements.snake";
+
 
 Map.load();
+Snake.generate();
 Map.draw();
